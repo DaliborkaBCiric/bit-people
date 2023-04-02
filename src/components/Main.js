aa77370a1943003357bb7ebdf7e6bfdeb616c2a2
Daliborka Ciric <bukica1989@gmail.com>
2023-04-02 18:52:09 +0200

    feature 11

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
 import User from "./User";
 import Search from "./Search";
+import NoResulst from "./NoResults";
 
 
 function Main(props) {
@@ -19,48 +20,50 @@ function Main(props) {
 		e = e.split(" ")
 		e = e.filter(e1 => e1.trim().length > 0);
 
-		
+
 		const firstName = props.users.filter(user => {
 			if (e === "") return props.users
-			return e.some( ew => user.name.first.toLowerCase().includes(ew.toLowerCase()) );
+			return e.some(ew => user.name.first.toLowerCase().includes(ew.toLowerCase()));
 		})
 
 		const user_array = firstName.length > 0 ? firstName : props.users
 
 		const lastName = user_array.filter(user => {
 			if (e === "") return props.users
-			return e.some( ew => user.name.last.toLowerCase().includes(ew.toLowerCase()) );
+			return e.some(ew => user.name.last.toLowerCase().includes(ew.toLowerCase()));
 		})
-		
+
 		const results = [...firstName, ...lastName]
 		let uniqueUsers = results.filter((element, index) => {
 			return results.indexOf(element) === index;
-	});
-		
+		});
+
 		setstate({
 			query: e,
 			users: e.length > 0 ? uniqueUsers : props.users
 		})
 	}
-
 	return (
 		<div className="container">
 
 			<Search value={state.query} handleChange={handleChange} />
-			{props.view === 'grid' ?
-				<div className="cards">
-					{state.users.map((user, index) => (
-						<div className="card" key={index}>
-							<UserCard key={index} user={user} />
-						</div>
-					))}
-				</div>
+			{state.users.length > 0 ?
+				props.view === 'grid' ?
+					<div className="cards">
+						{state.users.map((user, index) => (
+							<div className="card" key={index}>
+								<UserCard key={index} user={user} />
+							</div>
+						))}
+					</div>
+					:
+					<div>
+						{state.users.map((user, index) => (
+							<User key={index} user={user} />
+						))}
+					</div>
 				:
-				<div>
-					{state.users.map((user, index) => (
-						<User key={index} user={user} />
-					))}
-				</div>
+				<NoResulst />
 			}
 		</div>
 	);
