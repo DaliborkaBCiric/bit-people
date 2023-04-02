54a73728faab038121a9a995bb7df8cb5e241dd8
Daliborka Ciric <bukica1989@gmail.com>
2023-04-02 02:38:39 +0200

    feature 7

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,21 +3,25 @@ import User from "./User";
 
 function Main(props) {
 
-  return (
-    <div className="container">
-        <div className="cards">
-        {props.users.results?.map((user, index) => (
-            <div className="card" key={index}>
-							{props.view === 'grid' ? 
-                <UserCard key={index} user={user} />
-								:
-								<User key={index} user={user} />
-							}
-            </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			{props.view === 'grid' ?
+				<div className="cards">
+					{props.users.results?.map((user, index) => (
+						<div className="card" key={index}>
+							<UserCard key={index} user={user} />
+						</div>
+					))}
+				</div>
+				:
+				<div>
+					{props.users.results?.map((user, index) => (
+						<User key={index} user={user} />
+					))}
+				</div>
+			}
+		</div>
+	);
 }
 
 export default Main;
